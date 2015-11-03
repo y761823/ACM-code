c6e155d6e27d105fa0e642d0443a812628c592ed
y761823 <y761823@163.com>
2015-11-03 19:17:32 +0800

    HDOJ 5527 Too Rich

--- HDOJ/p5527.cpp
+++ HDOJ/p5527.cpp
@@ -11,6 +11,7 @@ const int INF = 0x3f3f3f3f;
 const int MOD = 1e9 + 7;
 
 int cost[] = {1, 5, 10, 20, 50, 100, 200, 500, 1000, 2000};
+int cccc[] = {1, 1, 1, 1, 2, 2, 2, 4, 4, 4};
 int c[MAXN];
 int T, p, n = 10;
 
@@ -18,8 +19,10 @@ LL solve(int i, LL rest) {
     if(rest == 0) return 0;
     if(i < 0) return INF;
     LL v = min((LL)c[i], rest / cost[i]);
-    LL res = v + solve(i - 1, rest - v * cost[i]);
-    if(v > 0) res = min(res, v - 1 + solve(i - 1, rest - (v - 1) * cost[i]));
+    LL res = INF;
+    for(int x = 0; x < cccc[i] && x <= v; ++x) {
+        res = min(res, v - x + solve(i - 1, rest - (v - x) * cost[i]));
+    }
     return res;
 }
 
