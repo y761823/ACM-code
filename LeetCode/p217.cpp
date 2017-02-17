739be681cd1c62603a25dec801e7882a856980c2
y761823 <y761823@163.com>
2017-02-17 17:50:45 +0800

    LeetCode 217 Contains Duplicate

---
+++ LeetCode/p217.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for (int x : nums)
+            if (st.find(x) != st.end()) return true;
+            else st.insert(x);
+        return false;
+    }
+};
