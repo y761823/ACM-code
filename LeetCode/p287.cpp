8976daee5f5cccfdcf208debb526c365952e90a7
y761823 <y761823@163.com>
2019-07-16 17:01:37 +0800

    LeetCode 287 Find the Duplicate Number

---
+++ LeetCode/p287.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast = nums[slow];
+        while (slow != fast)
+            slow = nums[slow], fast = nums[nums[fast]];
+        slow = 0;
+        while (slow != fast)
+            slow = nums[slow], fast = nums[fast];
+        return slow;
+    }
+};
